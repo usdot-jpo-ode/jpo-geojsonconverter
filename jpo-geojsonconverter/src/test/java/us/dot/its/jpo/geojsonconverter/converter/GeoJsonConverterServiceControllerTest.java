cd6e1ee5b04a9f599054e11d6332f97ae31decb3
Ivan Yourshaw <39739503+iyourshaw@users.noreply.github.com>
2022-10-25 15:34:43 -0600

    Integrating validators with unit tests

--- jpo-geojsonconverter/src/test/java/us/dot/its/jpo/geojsonconverter/converter/GeoJsonConverterServiceControllerTest.java
+++ jpo-geojsonconverter/src/test/java/us/dot/its/jpo/geojsonconverter/converter/GeoJsonConverterServiceControllerTest.java
@@ -4,12 +4,16 @@ import static org.junit.Assert.assertNotNull;
 
 import org.junit.Before;
 import org.junit.Test;
+import mockit.*;
 
 import us.dot.its.jpo.geojsonconverter.GeoJsonConverterProperties;
+import us.dot.its.jpo.geojsonconverter.validator.MapJsonValidator;
 
 public class GeoJsonConverterServiceControllerTest {
     GeoJsonConverterServiceController geoJsonConverterServiceController;
     GeoJsonConverterProperties props;
+
+    @Mocked MapJsonValidator mapJsonValidator;
 
     @Before
     public void setup() {
@@ -19,7 +23,7 @@ public class GeoJsonConverterServiceControllerTest {
 
     @Test
     public void testSpringBootLoaded() {
-        geoJsonConverterServiceController = new GeoJsonConverterServiceController(props);
+        geoJsonConverterServiceController = new GeoJsonConverterServiceController(props, mapJsonValidator);
         assertNotNull(geoJsonConverterServiceController);
     }
 }
