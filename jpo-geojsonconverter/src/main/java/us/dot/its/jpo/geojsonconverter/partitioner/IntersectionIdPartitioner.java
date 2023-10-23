a73104f7eaa1d7239646b19ac0f78e309eea9ba2
Ivan Yourshaw <39739503+iyourshaw@users.noreply.github.com>
2023-10-22 23:29:09 -0600

    Partition function for intersection id

--- jpo-geojsonconverter/src/main/java/us/dot/its/jpo/geojsonconverter/partitioner/IntersectionIdPartitioner.java
+++ jpo-geojsonconverter/src/main/java/us/dot/its/jpo/geojsonconverter/partitioner/IntersectionIdPartitioner.java
@@ -7,30 +7,32 @@ import us.dot.its.jpo.geojsonconverter.serialization.serializers.JsonSerializer;
 import java.nio.ByteBuffer;
 
 /**
- * Partitioner that partitions based only on Intersection ID for
- * objects that implement the {@link IntersectionKey} class.
+ * Partitioner that partitions based only on Intersection ID for objects that implement the {@link IntersectionKey} class.
  *
  * <p>Partitioning ignores the region, so that items with or without a region defined will be on the same partition.</p>
  *
- * <p>Objects that don't implement the {@link IntersectionKey} are partitioned on the entire serialized key, the same as the kafka default behavior.</p>
+ * <p>Objects that don't implement the {@link IntersectionKey} interface are partitioned on the hash of the entire
+ * serialized key, the same as the kafka default behavior.</p>
  */
 public class IntersectionIdPartitioner<K, V> implements StreamPartitioner<K, V> {
 
     @Override
     public Integer partition(String topic, K key, V value, int numPartitions) {
-        byte[] partitionBytes;
+
 
         if (key instanceof IntersectionKey) {
             var intersectionKey = (IntersectionKey)key;
             int intersectionId = intersectionKey.getIntersectionId();
-            partitionBytes = ByteBuffer.allocate(Integer.SIZE/8).putInt(intersectionId).array();
+            return intersectionId % numPartitions;
         } else {
+            byte[] partitionBytes;
             try (var serializer = new JsonSerializer<K>()) {
                 partitionBytes = serializer.serialize(topic, key);
             }
+            return Utils.toPositive(Utils.murmur2(partitionBytes)) % numPartitions;
         }
 
-        return Utils.toPositive(Utils.murmur2(partitionBytes)) % numPartitions;
+
     }
 
 }
