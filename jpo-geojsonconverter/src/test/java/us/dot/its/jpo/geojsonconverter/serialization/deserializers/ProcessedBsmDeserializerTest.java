2d280ddc344cefd9e39038817572927eee07cd69
Ivan Yourshaw <39739503+iyourshaw@users.noreply.github.com>
2025-01-06 12:16:49 -0700

    Add more conditions to deserializer test

--- jpo-geojsonconverter/src/test/java/us/dot/its/jpo/geojsonconverter/serialization/deserializers/ProcessedBsmDeserializerTest.java
+++ jpo-geojsonconverter/src/test/java/us/dot/its/jpo/geojsonconverter/serialization/deserializers/ProcessedBsmDeserializerTest.java
@@ -4,12 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.Resource;
 
-import static org.junit.Assert.assertNull;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.fail;
-
 import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,6 +12,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import us.dot.its.jpo.geojsonconverter.pojos.geojson.Point;
 import us.dot.its.jpo.geojsonconverter.pojos.geojson.bsm.ProcessedBsm;
+import us.dot.its.jpo.ode.plugin.j2735.J2735BitString;
+import us.dot.its.jpo.ode.plugin.j2735.J2735TransmissionState;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest({
@@ -50,7 +50,49 @@ public class ProcessedBsmDeserializerTest {
             ProcessedBsm<Point> bsm = serializer.deserialize("the_topic", bsmBytes);
             assertNotNull(bsm);
 
-            assertEquals("172.19.0.1", bsm.getProperties().getOriginIp());
+            var props = bsm.getProperties();
+            assertEquals("172.19.0.1", props.getOriginIp());
+            assertEquals(new BigDecimal("359.4"), props.getHeading());
+            assertEquals("12A7A951", props.getId());
+            assertEquals(25, props.getMsgCnt());
+            assertEquals(2800, props.getSecMark());
+            assertEquals(J2735TransmissionState.UNAVAILABLE, props.getTransmission());
+            var accelSet = props.getAccelSet();
+            var size = props.getSize();
+            assertEquals(208, size.getWidth());
+            assertEquals(586, size.getLength());
+            assertNotNull(accelSet);
+            assertEquals(new BigDecimal(2001), accelSet.getAccelLat());
+            assertEquals(new BigDecimal(0), accelSet.getAccelLong());
+            assertEquals(new BigDecimal(-127), accelSet.getAccelVert());
+            assertEquals(new BigDecimal(0), accelSet.getAccelYaw());
+            var accuracy = props.getAccuracy();
+            assertNotNull(accuracy);
+            assertEquals(new BigDecimal(5), accuracy.getSemiMajor());
+            assertEquals(new BigDecimal(2), accuracy.getSemiMinor());
+            assertEquals(new BigDecimal(0), accuracy.getOrientation());
+            var brakes = props.getBrakes();
+            assertNotNull(brakes);
+            assertEquals("unavailable", brakes.getTraction());
+            assertEquals("unavailable", brakes.getAbs());
+            assertEquals("unavailable", brakes.getScs());
+            assertEquals("unavailable", brakes.getBrakeBoost());
+            assertEquals("unavailable", brakes.getAuxBrakes());
+            J2735BitString wheelBrakes = brakes.getWheelBrakes();
+            assertNotNull(wheelBrakes);
+            assertFalse(wheelBrakes.get("leftFront"));
+            assertFalse(wheelBrakes.get("rightFront"));
+            assertTrue(wheelBrakes.get("unavailable"));
+            assertFalse(wheelBrakes.get("leftRear"));
+            assertFalse(wheelBrakes.get("rightRear"));
+            var geometry = bsm.getGeometry();
+            assertNotNull(geometry);
+            assertEquals("Point", geometry.getType());
+            var coords = geometry.getCoordinates();
+            assertNotNull(coords);
+            assertEquals(2, coords.length);
+            assertEquals(-105.0342901, coords[0]);
+            assertEquals(40.5671913, coords[1]);
             
         } catch (Exception e) {
             fail("Unexpected exception: " + e);
